20c1ca434eec5089d71c50c40c5942101ad64b5e
Namdev <137170414+Namdev03@users.noreply.github.com>
2026-06-25 20:25:58 +0530

    Update CORS origin for production environment

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://lopy-backend.vercel.app",
     credentials:true
 }))
 app.use('/user',userRouter);
